747527b80ac3acf1db26d96d8245b0663391918e
littlwoop <j.finkenstaedt@gmail.com>
2025-05-22 10:09:27 +0200

    adjust layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,18 @@
-
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default function Layout() {
+  const location = useLocation();
+  const isIndexPage = location.pathname === "/";
+
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <main className="flex-1">
         <Outlet />
       </main>
-      <Footer />
+      {isIndexPage && <Footer />}
     </div>
   );
 }
